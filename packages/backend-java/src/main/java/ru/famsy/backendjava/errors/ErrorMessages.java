f3740a848dcfdc36fc6b47d258206a1ca95d48b5
gogakler_dev <gogakler.development@gmail.com>
2024-12-20 19:20:00 +0300

    feat(errors-messages): add ErrorMessages class

---
+++ packages/backend-java/src/main/java/ru/famsy/backendjava/errors/ErrorMessages.java
@@ -0,0 +1,6 @@
+package ru.famsy.backendjava.errors;
+
+public class ErrorMessages {
+    public static final String REQUIRED_FIELD = "Поле обязательно для заполнения.";
+    public static final String NOT_VALID = "Поле не валидно";
+}
