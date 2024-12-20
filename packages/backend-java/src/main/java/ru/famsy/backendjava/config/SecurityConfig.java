28d90c75dbad09c6fc5809dd630288e1a885b54b
gogakler_dev <gogakler.development@gmail.com>
2024-12-20 19:19:25 +0300

    fix(security): fix security config

--- packages/backend-java/src/main/java/ru/famsy/backendjava/config/SecurityConfig.java
+++ packages/backend-java/src/main/java/ru/famsy/backendjava/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.famsy.backendjava.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -8,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
