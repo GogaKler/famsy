b0ed661256624d959312c2123e7bcdd6d0c511a5
gogakler_dev <gogakler.development@gmail.com>
2024-12-22 03:32:14 +0300

    feat(security): add security DEV config

---
+++ packages/backend/src/main/java/ru/famsy/backend/config/SecurityConstants.java
@@ -0,0 +1,12 @@
+package ru.famsy.backend.config;
+
+public class SecurityConstants {
+    public static final long ACCESS_TOKEN_EXPIRATION = 300000;
+    public static final long REFRESH_TOKEN_EXPIRATION = 604800000;
+    public static final String JWT_SECRET = "5e9366349290a82f53e81b2c359f9554a3c291b284e482ab3c3aab98d3e044b9";
+    public static final int MAX_DEVICES_PER_USER = 5;
+
+    private SecurityConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+}
