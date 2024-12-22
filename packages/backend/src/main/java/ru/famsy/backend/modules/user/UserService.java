752eed41eda3fb1937897ed3183b249471ec6006
gogakler_dev <gogakler.development@gmail.com>
2024-12-22 03:31:33 +0300

    fix(userService): fix syntax UserService

--- packages/backend/src/main/java/ru/famsy/backend/modules/user/UserService.java
+++ packages/backend/src/main/java/ru/famsy/backend/modules/user/UserService.java
@@ -11,46 +11,52 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private final ru.famsy.backend.modules.user.mapper.UserMapper userMapper;
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+  private final ru.famsy.backend.modules.user.mapper.UserMapper userMapper;
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
 
-    UserService(UserRepository userRepository, UserMapper userMapper) {
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
-        this.passwordEncoder = new BCryptPasswordEncoder();
-    }
+  UserService(UserRepository userRepository, UserMapper userMapper) {
+    this.userRepository = userRepository;
+    this.userMapper = userMapper;
+    this.passwordEncoder = new BCryptPasswordEncoder();
+  }
 
-    public UserEntity saveUser(UserEntity user) {
-        user.setPassword(hashPassword(user.getPassword()));
-        return this.userRepository.save(user);
-    }
+  public UserEntity saveUser(UserEntity user) {
+    user.setPassword(hashPassword(user.getPassword()));
+    return this.userRepository.save(user);
+  }
 
-    public List<UserEntity> findAllUsers() {
-        return userRepository.findAll();
-    }
+  public List<UserEntity> findAllUsers() {
+    return userRepository.findAll();
+  }
 
-    public UserEntity findUserById(Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Пользватель с таким id не найден:" + id));
-    }
+  public UserEntity findUserById(Long id) {
+    return userRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Пользватель с таким id не найден:" + id));
+  }
 
-    public UserEntity findUserByEmail(String email) {
-        return this.userRepository.findByEmail(email).orElse(null);
-    }
+  public UserEntity findUserByEmail(String email) {
+    return userRepository.findByEmail(email)
+            .orElseThrow(() -> new EntityNotFoundException("Пользватель с таким email не найден:" + email));
+  }
 
-    public UserEntity updateUser(Long id, UserEntity user) {
-        UserEntity userEntity = this.findUserById(id);
-        userMapper.updateUserEntity(user, userEntity);
-        return this.userRepository.save(userEntity);
-    }
+  public UserEntity findUserByUsername(String username) {
+    return userRepository.findByUsername(username)
+            .orElseThrow(() -> new EntityNotFoundException("Пользватель с таким username не найден:" + username));
+  }
 
-    public void deleteUser(Long id) {
-        this.userRepository.deleteById(id);
-    }
+  public UserEntity updateUser(Long id, UserEntity user) {
+    UserEntity userEntity = findUserById(id);
+    userMapper.updateUserEntity(user, userEntity);
+    return userRepository.save(userEntity);
+  }
 
-    private String hashPassword(String password) {
-        return passwordEncoder.encode(password);
-    }
+  public void deleteUser(Long id) {
+    userRepository.deleteById(id);
+  }
+
+  private String hashPassword(String password) {
+    return passwordEncoder.encode(password);
+  }
 }
