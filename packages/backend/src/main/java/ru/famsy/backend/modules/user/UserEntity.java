034cecea769e52e6b0bbdbad1c1bc8a9abb0cde4
gogakler_dev <gogakler.development@gmail.com>
2024-12-26 20:26:58 +0300

    feat(migrations): add liquibase. add rename and audit migration for users

--- packages/backend/src/main/java/ru/famsy/backend/modules/user/UserEntity.java
+++ packages/backend/src/main/java/ru/famsy/backend/modules/user/UserEntity.java
@@ -1,6 +1,9 @@
 package ru.famsy.backend.modules.user;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import ru.famsy.backend.common.entity.BaseEntity;
 
 @Entity
 @Table(name = "users")
@@ -8,32 +11,31 @@ public class UserEntity extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String username;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     @Column(nullable = false, unique = true)
     private String email;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Column(nullable = false)
     private String password;
 
+    public UserEntity() {}
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
