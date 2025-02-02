6fa25b643c400dbde83206f21d9ce0cd19a37c43
raketa2332 <ryzhdan2002@gmail.com>
2025-02-02 19:34:08 +0300

    feat(playwright): update LoginPage

--- e2e/pages/login-page.ts
+++ e2e/pages/login-page.ts
@@ -1,9 +1,42 @@
 import {BasePage} from "./base-page";
-import {Page} from "@playwright/test";
+import {Page, Locator} from "@playwright/test";
 
-export class LoginPage extends BasePage{
+// @ts-ignore
+export class LoginPage extends BasePage {
+    private readonly loginInputID = "loginInput";
+    private readonly passwordInputID = "passwordInput";
+    private readonly loginButtonID = "loginButton";
+    private readonly forgotPasswordButtonID = "forgotPasswordButton";
+    private readonly rememberMeCheckboxID = "rememberMeCheckbox";
+    private readonly registrationButtonID = "registrationButton";
+
     constructor(page: Page) {
         super(page);
-
     }
+    get loginInput():Locator{
+        return this.page.getByTestId(this.loginInputID);
+    }
+    get passwordInput(): Locator{
+        return this.page.getByTestId(this.passwordInputID);
+    }
+    get loginButton(): Locator{
+        return this.page.getByTestId(this.loginButtonID);
+    }
+    get forgotPasswordButton(): Locator{
+        return this.page.getByTestId(this.forgotPasswordButtonID);
+    }
+    get rememberMeCheckbox(): Locator{
+        return this.page.getByTestId(this.rememberMeCheckboxID);
+    }
+    get registrationButton(): Locator{
+        return this.page.getByTestId(this.registrationButtonID);
+    }
+
+    async login(login: string, password: string) {
+        await this.loginInput.fill(login);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+
+
 }
