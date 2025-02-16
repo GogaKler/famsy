48f718a8acb9d2c661064afed41d2d76c80760e8
GinsenG-dev <evgeniy.zok@mail.ru>
2025-02-16 21:28:15 +0300

    chore: resolve review
    Refs: #24

--- frontend/src/entities/user/ui/user-avatar/UserAvatar.vue
+++ frontend/src/entities/user/ui/user-avatar/UserAvatar.vue
@@ -3,13 +3,13 @@ import { AuthService } from '@entities/auth';
 import { container } from 'tsyringe';
 import { computed } from 'vue';
 const authService: AuthService = container.resolve(AuthService);
-const currentUser = authService.authStore.currentUser?.usernameInitials;
 
-const initials = computed(() => {
-  return `${currentUser}`.toUpperCase();
+const usernameInitials = computed(() => {
+  return authService.authStore.currentUser?.usernameInitials;
 });
+
 </script>
 
 <template>
-  <FamsyAvatar :label="initials" shape="circle" class="cursor-pointer" />
+  <FamsyAvatar :label="usernameInitials" shape="circle" class="cursor-pointer" />
 </template>
