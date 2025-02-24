2eab220a10cc714010ea188891e9daf0cc51d05b
gogakler_dev <gogakler.development@gmail.com>
2025-02-25 00:33:31 +0300

    chore: update neutral colors

--- frontend/src/widgets/header/HeaderLayout.vue
+++ frontend/src/widgets/header/HeaderLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="bg-surface-secondary">
+  <header class="bg-surface-primary">
     <div class="flex items-center justify-between px-4 pt-4 pb-2">
       <div class="flex items-center gap-5">
         <slot name="left" />
