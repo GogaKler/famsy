fc57fef5fce8c009ca8511a6fd36d46e9b409abc
gogakler_dev <gogakler.development@gmail.com>
2025-04-29 19:48:26 +0300

    init frontend-native

---
+++ frontend-native/app/index.tsx
@@ -0,0 +1,22 @@
+// import { Text, View } from "react-native";
+//
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Edit app/index.tsx to edit this screen.</Text>
+//     </View>
+//   );
+// }
+
+
+function someFoo() {
+  setTimeout(() => {
+    console.log('foo');
+  }, 1000)
+}
